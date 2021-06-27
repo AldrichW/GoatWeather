5e377ef17c4418c30de08cdc0a3c6f8f512d1349
Aldrich Wingsiong <aldrichw@AldrichMBP.local>
2021-06-27 15:10:14 -0700

    [GoatWeather] Add APISecrets file with a placeholder value

---
+++ GoatWeather/APISecrets.swift
@@ -0,0 +1,12 @@
+//
+//  APISecrets.swift
+//  GoatWeather
+//
+//  Created by Aldrich Wingsiong on 6/25/21.
+//
+
+import Foundation
+
+class APISecrets {
+    static let openWeatherAPIKey = "<ADD_YOUR_API_KEY>"
+}
